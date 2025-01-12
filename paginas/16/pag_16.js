7c2c14459b78b0ec6066b99d31f7cffee0525e54
ZadeBoSS <164022135+ZadeBoSS@users.noreply.github.com>
2025-01-12 20:24:04 +0100

    Update and rename pag16.js to pag_16.js

--- paginas/16/pag_16.js
+++ paginas/16/pag_16.js
@@ -5,7 +5,7 @@ const problemas = [
         titulo: "Factor Común",
         operaciones: [
             { pregunta: "3·b + 5·b – 2·b", respuesta: "6·b" },
-            { pregunta: "6⁴ + 3⁴ + 2⁴", respuesta: "11x4" },
+            { pregunta: "7·a - 3·a + 5·a", respuesta: "9·a" },
             { pregunta: "6·a + 6·b", respuesta: "6·(a + b)" },
             { pregunta: "2·a + 2·c", respuesta: "2·(a + c)" }
         ],
@@ -19,7 +19,7 @@ const problemas = [
             { pregunta: "2306 x 305", respuesta: "703330" },
             { pregunta: "(-2) · (-5) · (-9)", respuesta: "-90" },
             { pregunta: "4 · (-5) · 2", respuesta: "-40" },
-            { pregunta: "3 · (-3) + 4 · (-2) + (-4) · (-5)", respuesta: "3" }
+            { pregunta: "3 · (-3) + 4 · (-2) + (-4) · (-5)", respuesta: "5" }
         ],
         pista: "Ten cuidado con los signos en las multiplicaciones.",
         explicacion: "Recuerda que multiplicar números negativos sigue reglas específicas."
@@ -31,9 +31,9 @@ const problemas = [
             { pregunta: "55 + 23 + 15 = 55 + 15 + 23", respuesta: "Conmutativa" },
             { pregunta: "7 · 25 · 4 = 7 · (25 · 4)", respuesta: "Asociativa" },
             { pregunta: "5 · (8 + 18) = 5 · 8 + 5 · 18", respuesta: "Distributiva" },
-            { pregunta: "87 − 23 = 84 − 20 = 64", respuesta: "No aplica ninguna de las propiedades clásicas (es una operación directa)." }
+            { pregunta: "87 − 23 = 84 − 20 = 64", respuesta: "Ninguna" }
         ],
-        pista: "Identifica cómo cambian el orden o la agrupación. Recuerda los tipos de propiedades:(Conmutativa, Asociativa y Distributiva)",
+        pista: "Identifica cómo cambian el orden o la agrupación. Recuerda los tipos de propiedades:(Conmutativa, Asociativa y Distributiva o Ninguna)",
         explicacion: "Las propiedades indican cómo operar los números manteniendo el resultado."
     }
 ];
